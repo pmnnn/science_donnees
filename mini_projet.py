76beafa3450c70100bb6fb82ba8f983df1a672ed
pmnnn <101646796+pmnnn@users.noreply.github.com>
2023-12-15 01:33:42 +0100

    Update mini_projet.py

--- mini_projet.py
+++ mini_projet.py
@@ -1,6 +1,7 @@
 import statistics as stat
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
 
 """
 EXERCICE4
@@ -15,17 +16,14 @@ def boite_dispersion(serie, titre="Boîte de Dispersion"):
     ax.set_ylabel("Valeurs")
     plt.show()
 
-def  tracer_histogramme(liste_longueurs, titre="Histogramme des longueurs de la rectrice centrale"):
-    min_BonasaUmbellus = min(liste_longueurs)
-    max_BonasaUmbellus = max(liste_longueurs)
-    plt.hist(liste_longueurs, bins = max_BonasaUmbellus-min_BonasaUmbellus, alpha = 0.5, color = 'blue', edgecolor = 'black')
+def  tracer_histogramme(liste_longueurs, titre, abcisse, n):
+    plt.hist(liste_longueurs, bins = n, alpha = 0.5, color = 'blue', edgecolor = 'black')
 
     plt.title(titre)
-    plt.xlabel('Longueur (cm)')
+    plt.xlabel(abcisse)
     plt.ylabel('Effectif')
-
-    plt.show()
     
+
 
 BonasaUmbellus = [153,165,160,150,159,151,163,160,158,150,154,153,163,150,158,150,158,155,163,156,157,162,160,152,164,158,153,162,166,162,165,157,174,158,171,162,155,156,159,162,152,158,164,164,162,158,156,171,164,158]
 
@@ -42,7 +40,7 @@ ecart_quartiles_BonasaUmbellus = quartiles_BonasaUmbellus[3]-quartiles_BonasaUmb
 variance_BonasaUmbellus = np.var(BonasaUmbellus)
 ecart_type_BonasaUmbellus = np.std(BonasaUmbellus)
 
-
+print("EXERCICE 4:")
 print("La valeur longueur minimun de l'échantillon est de ", min_BonasaUmbellus, "cm")
 print("La valeur longueur maximun de l'échantillon est de ", max_BonasaUmbellus, "cm")
 print("L'étendue de l'échantillon est de ", etendue_BonasaUmbellus, "cm")
@@ -53,7 +51,64 @@ print("Variance = ", variance_BonasaUmbellus)
 print("Ecart type = ", ecart_type_BonasaUmbellus)
 
 boite_dispersion(BonasaUmbellus)
-tracer_histogramme(BonasaUmbellus)
+tracer_histogramme(BonasaUmbellus, "Histogramme des longueurs de la rectrice centrale", 'Longuer(cm)', etendue_BonasaUmbellus)
+plt.show()
+
+
+
+
+"""
+EXERCICE 5
+"""
+
+
+def importer_csv(nom_fichier):    
+    data = []
+    with open(nom_fichier, 'r') as fichier_csv:
+        lecteur_csv = csv.reader(fichier_csv)
+        for ligne in lecteur_csv:
+            for i in ligne:
+                data.append(float(i))
+    return data
+
+
+
+S1 = importer_csv('data.csv')
+min_S1 = min(S1)
+max_S1 = max(S1)
+etendue_S1 = max(S1)-min(S1)
+
+POIDS1 = S1[0:15]
+m1 = stat.mean(POIDS1)
+
+POIDS2 = S1[15:-1]
+m2 = stat.mean(POIDS2)
+
+mtot = (m1+m2)/2
+
+
+
+moyenne_S1 = stat.mean(S1)
+ecart_type_S1 = np.std(S1)
+
+
+print("EXERCICE 5:")
+print("La moyenne de l'échantillon est de ", moyenne_S1, "kg")
+print("Ecart type = ", ecart_type_S1)
+print("m1 = ", m1)
+print("m2 = ", m2)
+print("mtot = ", mtot)
+
+boite_dispersion(S1)
+tracer_histogramme(S1, "Diagramme en bâtons des pesées des 30 bébés", "Poids en kg", [1.8,1.9, 2., 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.,  3.1, 3.2, 3.3, 3.4, 3.5,3.6, 3.7, 3.8])
+plt.xticks([1.8, 2., 2.2, 2.4, 2.6, 2.8, 3., 3.2, 3.4,3.6, 3.8])
+plt.show()
+
+
+
+
+
+
 
 
 
